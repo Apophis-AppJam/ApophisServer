b37c35d06d2b4bcda9ddf7c3929c38bbab3a7185
sophryu99 <sehwa_ryu@berkeley.edu>
2021-01-02 18:36:34 +0900

    [UPDATE] CHAT_TB column deleted

--- apophis/controller/choiceController.js
+++ apophis/controller/choiceController.js
@@ -7,15 +7,19 @@ const sc = require('../modules/statusCode');
 const choiceService = require('../service/choiceService')
 
 module.exports = {
-    // 선택 창 종류와 그에 따른 스트링 값 내려주기
+    
+    /* 대화창 상세 조회  GET : [ /choice/:day/:chatDetailsId] */
     readChat: async (req, res) => {
+        const day = req.params.day;
+        const chatActionId = req.params.chatActionId;
+        console.log(day)
         try {
-            const comments = await choiceService.getChoice();
+            const comments = await choiceService.getChoice(day, chatActionId);
             if (!comments) {
-                console.log('comments 테이블이 비어있습니다');
+                console.log('Choice 테이블이 비어있습니다');
                 return res.status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
             }
-            return res.status(sc.OK).send(ut.success(sc.OK, "대화창 내용 조회 성공", comments));
+            return res.status(sc.OK).send(ut.success(sc.OK, "대화창 상세 조회 성공", comments));
         } catch (error) {
             console.error(error);
             return res.status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
--- apophis/service/choiceService.js
+++ apophis/service/choiceService.js
@@ -2,31 +2,22 @@ const { ChoiceWords, ChatDetails } = require('../models');
 
 module.exports = {
     
-    getChoice: async () => {
+    getChoice: async (day, chatActionId) => {
         try {
             const aponymousChat = await ChatDetails.findAll({
-                // where : {
-                //     ChoiceWordsIdx: 1,
-                // },
-                attributes: ['ChatDetailsIdx', 'info', 'chatAction', 'chatView'],
+                where : {
+                    day: day,
+                    chatAction : chatActionId
+                },
+                attributes: ['ChatDetailsIdx', 'info', 'chatAction', 'chatView', 'day'],
+                include: [{
+                    model: ChoiceWords,
+                    attributes: ['choiceWords']
+                }]
             });
             return aponymousChat;
         } catch (error) {
             throw error;
         }
     },
-
-    // getNewCreators: async () => {
-    //     try {
-    //         const creators = await Creator.findAll({
-    //             where: {
-    //                 ifNew: 1,
-    //             },
-    //             attributes: ['profileUrl'],
-    //         });
-    //         return creators;
-    //     } catch (error) {
-    //         throw error;
-    //     }
-    // }
 }
