df0c151f9ce9883a40bedcc3548182ad7d894e75
Suzie Lee <wnsuwls0707@gmail.com>
2021-01-02 22:42:57 +0900

    [Add] POST reply

--- apophis/routes/reply/index.js
+++ apophis/routes/reply/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const replyController = require('../../controller/replyController')
+
+router.post('/', replyController.getReply);
 module.exports = router;
