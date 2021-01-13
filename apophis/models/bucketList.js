598fe7df11674fa3c4fad2e2fc93fb4f3216b3a2
Suzie Lee <wnsuwls0707@gmail.com>
2021-01-13 23:12:17 +0900

    [Add] 버킷리스트, 편지 모델 추가

--- apophis/models/bucketList.js
+++ apophis/models/bucketList.js
@@ -1,27 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Chat', {
-        ChatIdx: {
+    return sequelize.define('BucketList', {
+        BucketListIdx: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             autoIncrement: true,
             primaryKey: true,
         },
-        text: {
-            type: DataTypes.STRING(200),
+        title: {
+            type: DataTypes.STRING(100),
             unique: false,
             allowNull: true,
             defaultValue: ''
         },
-        nextAction: {
-            type: DataTypes.STRING(40),
+        text: {
+            type: DataTypes.TEXT,
             unique: false,
             allowNull: true,
             defaultValue: ''
         }
         
     }, {
-        //모델의 옵션들을 지정하는곳    
-        tableName: 'CHAT_TB',
-        timestamps: false,
+        //모델의 옵션들을 지정하는곳 
+        tableName: 'BUCKET_LIST_TB',  
+        timestamps: true,
 
     });
 };
